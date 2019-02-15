aadf44848e5509c612ef0c241d64d6740a6762aa
David Oliveira <davidoliveira@Davids-MacBook-Pro.local>
2019-02-15 10:30:25 -0200

    Wording update

---
+++ docs/_next/static/webpack/static/development/pages/restructuring.js.e929bd4c98ee2f6f584e.hot-update.js
@@ -0,0 +1,54 @@
+webpackHotUpdate("static/development/pages/restructuring.js",{
+
+/***/ "./components/Restructuring/FirstSection.js":
+/*!**************************************************!*\
+  !*** ./components/Restructuring/FirstSection.js ***!
+  \**************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var animate_css_animate_min_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! animate.css/animate.min.css */ "./node_modules/animate.css/animate.min.css");
+/* harmony import */ var animate_css_animate_min_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(animate_css_animate_min_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _TextComponent__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../TextComponent */ "./components/TextComponent.js");
+var _jsxFileName = "/Users/davidoliveira/projects/terapartners-website/components/Restructuring/FirstSection.js";
+
+
+
+
+var Wrapper = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.withConfig({
+  displayName: "FirstSection__Wrapper",
+  componentId: "sc-781m1c-0"
+})([""]);
+
+function FirstSection()
+/* props */
+{
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Wrapper, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_TextComponent__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    title: "Restructuring",
+    subtitle: "We restructure stuff.",
+    text: "We are a boutique advisory firm specializing in cross-border mergers and acquisitions of innovative companies. With extensive deal-making experience, we pay a great deal of attention to details and leverage our high-quality relationships with CEOs, corporate development executives, and private equity firms worldwide to assist our clients in accelerating their growth and exploring strategic options in today\u2019s world, where technology changes in the blink of an eye. Organised in 4 Divisions: Mergers and Acquisitions, Restructuring, Consulting and Startups, the company operates globally providing services to mid-market to fortune 500 companies.",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (FirstSection);
+
+/***/ })
+
+})
+//# sourceMappingURL=restructuring.js.e929bd4c98ee2f6f584e.hot-update.js.map
