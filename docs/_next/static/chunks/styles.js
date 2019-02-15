9d1ad63d8b3c098a8ef9c834bdb5b29c71bf2b36
David Oliveira <davidcoliveira@gmail.com>
2019-02-15 10:44:27 -0200

    Wording update

--- docs/_next/static/chunks/styles.js
+++ docs/_next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1550234291199");
+            injectCss(link, link.href.split("?")[0] + "?unix=1550234636785");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1550234291552");
+            injectCss(link, link.href.split("?")[0] + "?unix=1550234637141");
           });
       }
     }
