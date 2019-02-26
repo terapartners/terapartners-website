32c079b2d853c4e3ad5223ee3b871e1be1719273
Pedro Victor <pedro.victor@gmail.com>
2019-02-26 18:10:38 -0300

    Sort partners by first name

--- components/Home/SecondSection.js
+++ components/Home/SecondSection.js
@@ -17,12 +17,12 @@ function SecondSection() {
         columns={3}
         srcs={['/static/pedromug.png', '/static/davidmug.png', '/static/fabianomug.png']}
         overlaySrcs={['/static/pedromugover.png', '/static/davidmugover.png', '/static/fabianomugover.png']}
-        names={['Pedro', 'David', 'Fabiano']}
+        names={['David', 'Fabiano', 'Pedro']}
         surnames={['Cavalcante', 'Oliveira', 'Mansur']}
         contents={[
-          'Financial and tech entrepreneur with 15+ years experience in high technology and banking. Founded FINTTA and was a tech advisor to VYA, Routeasy and Estudando.com. BS in Computer Engineering (Poli-USP)',
           'Executive and entrepreneur with 15+ years experience in banking, management consulting and financial advisory. Has held leadership positions at Suzano, Alix Partners, Olimpia Partners, Standard Bank, Banco Brasil Plural and Stern Stewart. MSc in Business (Stanford), MSc in Economics (Insper), BA in Business (USP), CFA charterholder.',
           'Accomplished finance executive with 15+ years experience in banking, private equity, restructuring and financial advisory. Partner at Banco Brasil Plural responsible for the real estate sector, Co-founded SPF Investimentos and has held leadership positions at Matlin Patterson and Banco Itaú BBA. BA in Business (Insper, graduated summa cum laude).',
+          'Financial and tech entrepreneur with 15+ years experience in high technology and banking. Founded FINTTA and was a tech advisor to VYA, Routeasy and Estudando.com. BS in Computer Engineering (Poli-USP)',
         ]}
       />
     </Wrapper>
